15a2ddd747324f08bd26191df12fe6f10b124197
Sanjar <theesanjar@gmail.com>
2024-09-30 10:08:11 +0500

    relation one to many

--- routes/web.php
+++ routes/web.php
@@ -4,11 +4,21 @@ use Illuminate\Support\Facades\Auth;
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AuthController;
+use App\Models\Post;
+use App\Models\User;
 
 Route::get('/', function () {
-    $user = Auth::user();
-    return view('welcome', compact('user'));
-})->name('home')->middleware('auth');
+
+    $user = User::find(2); // auth user
+    $post = Post::findOrFail(3); // ozining posti
+    if ($user->id != $post->user_id) {
+        return 'Siz faqat o\'zingizning postingizni o\'chira olasiz';
+    }
+    $post->delete();
+
+
+    return 'Post yangilandi';
+})->name('home');
 Route::get('/register',[AuthController::class,'registerForm'])->name('registerForm');
 
 // login routes
